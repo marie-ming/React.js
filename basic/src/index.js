21a2840b07fdf08d65b7290faf4d3b5c7869c44e
ming <shining4515@naver.com>
2023-05-17 18:07:08 +0900

    feat : 5강 - 로딩,에러 상태 추가

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppMontorsButton from './AppMontorsButton';
+import AppProducts from './basic/AppProducts';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMontorsButton />
+    <AppProducts />
   </React.StrictMode>
 );
 
