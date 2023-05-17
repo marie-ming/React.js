40b51d9805b19b9e8279547f14631207b7915c07
ming <shining4515@naver.com>
2023-05-17 18:27:49 +0900

    feat : 5강 - 클래스 컴포넌트

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppProducts from './basic/AppProducts';
+import AppClass from './AppClass';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <AppClass />
   </React.StrictMode>
 );
 
