568cb642402a3f84be13c7f3ad6cf7bd652d17b8
marie-ming <mingji408@gmail.com>
2023-05-22 00:24:54 +0900

    [#7] feat : 다크모드 설정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function App() {
   const [cate, setCate] = useState(category[0]);
 
   return (
-    <>
+    <DarkModeProvider>
       <Header category={category} cate={cate} setCate={setCate} />
       <TodoList cate={cate} />
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { useDarkMode } from '../../context/DarkModeContext';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
 const Header = ({ category, cate, setCate }) => {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button className={styles.toggle} onClick={toggleDarkMode}>
+        {!darkMode ? <HiMoon /> : <HiSun />}
+      </button>
       <ul className={styles.cateList}>
         {category.map((item, idx) => (
           <li key={idx}>
